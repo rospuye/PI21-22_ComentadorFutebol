284bd4dc245802a3123ec2b3902cb71c4a6c3de8
AfonsoCampos971 <afonso.campos@ua.pt>
2022-04-16 22:38:04 +0100

    labelling working needs testing in different computer

---
+++ log_filter/label_game.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import pandas as pd
+
+def get_index(time_array, time):
+    for i in range(len(time_array)):
+        if time_array[i] > time:
+            return i
+    return len(time_array)-1
+
+def label_game(labels_file, feature_file):
+    legend = ["nothing","kick_off","dribble","long_pass","short_pass","corner_shot","intersect","aggression","defense","poste","trave"]
+
+    labelling = np.array(pd.read_csv("labelling/input/"+labels_file,header=None))
+    game_features = np.array(pd.read_csv("logs/output/"+feature_file,header=None))
+    time_array = game_features[:,0]
+
+    # all output starts as 0 (nothing)
+    zeros = np.zeros(game_features.shape[0]).reshape(game_features.shape[0],1)
+    game_features = np.c_[game_features, zeros]
+
+    for line in labelling:
+        label = 0
+        initial = float(line[0].split(":")[0])*60+float(line[0].split(":")[1])
+        final = float(line[1].split(":")[0])*60+float(line[1].split(":")[1])
+        i = get_index(time_array, initial) 
+        f = get_index(time_array, final)
+        if(line[2].strip() in legend):
+            label = legend.index(line[2].strip())
+            game_features[i:f+1,-1] = label
+        else:
+            print("Unrecognized label:",line[2].strip())
+
+    #np.savetxt(("labelling/output/"+feature_file).rstrip(".csv")+"_labelled.csv", game_features, delimiter=",")
+    pd.DataFrame(game_features).to_csv(("labelling/output/"+feature_file).rstrip(".csv")+"_labelled.csv", index=False)
+
+if __name__ == "__main__":
+    label_game(sys.argv[1], sys.argv[2])
