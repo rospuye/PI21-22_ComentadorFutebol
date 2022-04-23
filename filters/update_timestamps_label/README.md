ed31e45ba9e50bcfcbebb7a1921bda31e2e6375f
itstar <luciusviniciusf@gmail.com>
2022-04-23 12:11:25 +0100

    update timestamps with seond match

--- filters/update_timestamps_label/README.md
+++ filters/update_timestamps_label/README.md
@@ -1,5 +1,8 @@
 # Update Timestamps
 Given a text file with the labelling structure and the video time that the game actually starts and receive an updated version of the file
 ```
-python update_timestamps.py <filename> <time_value> 
+python update_timestamps.py <filename> <time_offset (M:SS.MSMS)> <second_match (boolean)>
 ```
+OBS.: The time offset is the VIDEO TIME, not the GAME TIME. Put this value at the time that the tag "kick_off" appears on the top left.
+
+The second_match parameters just adds 5 min to the times (all games I think have that property)
--- filters/update_timestamps_label/update_timestamps.py
+++ filters/update_timestamps_label/update_timestamps.py
@@ -1,4 +1,5 @@
 import sys
+from tokenize import String
 
 def update_time(minu,sec, off_min, off_sec):
 
@@ -15,12 +16,23 @@ def update_time(minu,sec, off_min, off_sec):
 
     return new_min, new_sec
 
+def add_time(time1 : str, time2 : str):
+    # print(f"{time1 = }")
+    # print(f"{time2 = }")
+
+    time1_minute, time1_seconds = split_time(time1)
+    time2_minute, time2_seconds = split_time(time2)
+    new_time_seconds = round((time1_seconds + time2_seconds) % 60, 2)
+    new_time_minutes = time1_minute + time2_minute
+    if round(time1_seconds + time2_seconds, 2) > 60:
+        new_time_minutes += 1
+    return new_time_minutes, new_time_seconds
 
 def split_time(time : str):
     splt = time.split(":")
     return int(splt[0]), float(splt[1])
 
-def update_line(line : str, time : str):
+def update_line(line : str, time : str, isSecondMatch : bool):
     splt = line.split(',')
     off_min, off_sec = split_time(time)
     start_min, start_sec = split_time(splt[0])
@@ -28,20 +40,36 @@ def update_line(line : str, time : str):
 
     new_start_min, new_start_sec = update_time(start_min, start_sec, off_min, off_sec)
     new_end_min, new_end_sec = update_time(end_min, end_sec, off_min, off_sec)
-    output = f"{new_start_min}:{new_start_sec},{new_end_min}:{new_end_sec},"
-    for i in range(2, len(splt)):
+
+    if (isSecondMatch):
+        start_tupl = add_time(f"{new_start_min}:{new_start_sec}", "05:00.00")
+        end_tupl = add_time(f"{new_end_min}:{new_end_sec}", "05:00.00")
+        output = f"{start_tupl[0]}:{start_tupl[1]},{end_tupl[0]}:{end_tupl[1]},"
+    
+    else:
+        output = f"{new_start_min}:{new_start_sec},{new_end_min}:{new_end_sec},"
+    
+    for i in range(2, len(splt)): # complete the output with everything it had before
         output += splt[i]
     return output
 
 
-
 def main():
-    if len(sys.argv) < 3:
-        print("USAGE: python update_timestamps.py <filename> <time_value (M:SS.MSMS)>")
+    if len(sys.argv) < 4:
+        print("USAGE: python update_timestamps.py <filename> <time_offset (M:SS.MSMS)> <second_match (boolean)>")
         return
     
     filename = sys.argv[1]
     start_time = sys.argv[2]
+    isSecondMatch = sys.argv[3].lower() == "true"
+
+    # if isSecondMatch.lower() == "true":
+    #     print("is SecondMatch")
+    #     new_time_minutes, new_time_seconds = add_time(start_time, "05:00.00")
+    #     start_time = f"{new_time_minutes}:{new_time_seconds}"
+    #     print(f"{start_time = }")
+
+
 
     with open(filename, "r") as inpt:
         with open(f"{filename}_updated", "w") as outp:
@@ -50,7 +78,7 @@ def main():
             count = 1
             while next_line:
                 try:
-                    line = update_line(next_line, start_time)
+                    line = update_line(next_line, start_time, isSecondMatch)
                     outp.write(line)
                     next_line = next(inpt)
                     count += 1
@@ -59,7 +87,8 @@ def main():
                 except Exception:
                     print(f"ERROR PARSING LINE {count}: '{next_line}'. PLEASE CHECK IT AND TRY AGAIN")
                     break
+            print(f"File converted to {filename}_updated!")
 
-
+# 05:07.42
 if __name__ == "__main__":
     main()
