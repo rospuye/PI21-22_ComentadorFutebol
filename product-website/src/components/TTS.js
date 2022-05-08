6edbe6521862a60968b7b9c1dbd4ce9740c5c02d
itstar <luciusviniciusf@gmail.com>
2022-05-08 17:39:40 +0100

    still not working how it should

--- product-website/src/components/TTS.js
+++ product-website/src/components/TTS.js
@@ -1,38 +1,97 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const TTS = ({text, rate=5, volume=50, pitch=5}) => {
+// const TTS = ({text, rate=1, volume=50, pitch=1,
+// play, setPlay, setReady}) => {
+
+//     // Play: When true, the program starts playing. Maybe needs to be set as false after start playing
+//     // Pause: samething but for pause
+//     // Cancel: samething but for cancel
+//     // Resume: samething but for resume
+
+//     let speech = new SpeechSynthesisUtterance();
+//     speech.lang = "en"
+
+//     const [voices, setVoices] = useState([])
+
+//     useEffect(() => {
+//         window.speechSynthesis.onvoiceschanged = () => {
+//             setVoices(window.speechSynthesis.getVoices())
+//             setReady(true)
+//         }
+//     }, [])
+
+//     useEffect(() => {
+//         speech.text = text
+//         speech.rate = rate
+//         speech.volume = volume
+//         speech.pitch = pitch
+//     }, [text, rate, volume, pitch])
+
+//     useEffect(() => {
+//         console.log("voices", voices)
+//     }, [voices])
+
+//     useEffect(() => {
+//         console.log("init")
+//         if (!play) return
+//         console.log("play")
+//         window.speechSynthesis.speak(speech)
+//         setPlay(false)
+//     }, [play])
+
+//     return (
+//         <>
+//         </>
+//     )
+// }
+
+// export default TTS
+
+
+class TTS {
 
     // Play: When true, the program starts playing. Maybe needs to be set as false after start playing
     // Pause: samething but for pause
     // Cancel: samething but for cancel
     // Resume: samething but for resume
 
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en"
-
-    const [voices, setVoices] = useState([])
-
-    useEffect(() => {
+    constructor(setHasLoadedVoices, rate=1, volume=50, pitch=1) {
+        this.speech = new SpeechSynthesisUtterance();
+        this.speech.rate = rate
+        this.speech.volume = volume
+        this.speech.pitch = pitch
+        this.speech.lang = "en"
+        this.voices = []
+        this.selVoice = undefined
         window.speechSynthesis.onvoiceschanged = () => {
-            setVoices(window.speechSynthesis.getVoices())
+            console.log("i should had voices")
+            setHasLoadedVoices(true)
+            this.voices = window.speechSynthesis.getVoices()
+            this.selVoice = this.voices[0]
+            this.speech.voice = this.selVoice
         }
-    }, [])
+    }
 
-    useEffect(() => {
-        speech.text = text
-        speech.rate = rate
-        speech.volume = volume
-        speech.pitch = pitch
-    }, [text, rate, volume, pitch])
+    setText(text) {
+        this.speech.text = text
+    }
+    setRate(rate) {
+        this.speech.rate = rate
+    }
+    setVolume(volume) {
+        this.speech.volume = volume
+    }
+    setPitch(pitch) {
+        this.speech.pitch = pitch
+    }
 
-    useEffect(() => {
-        console.log("voices", voices)
-    }, [voices])
+    play() {
+        console.log("class", this.speech)
+        // window.speechSynthesis.cancel()
+        window.speechSynthesis.speak(this.speech)
+    } 
 
-    return (
-        <>
-        </>
-    )
+
 }
 
 export default TTS
--- product-website/src/pages/TTSTest.js
+++ product-website/src/pages/TTSTest.js
@@ -1,12 +1,64 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import TTS from "../components/TTS"
 
 const TTSTest = () => {
+
+    const [hasLoadedVoices, setHasLoadedVoices] = useState(false)
+    const [tts, setTts] = useState(new TTS(setHasLoadedVoices))
+
+    const areVoicesLoaded = () => {
+        if (hasLoadedVoices) return true
+        setTimeout(areVoicesLoaded, 1000)
+    }
+
+    useEffect(() => {
+        let msg = new SpeechSynthesisUtterance()
+        msg.text = "Hello people"
+        console.log("msg empty", msg)
+        window.speechSynthesis.speak(msg)
+        areVoicesLoaded()
+        // setTimeout(() => {
+        //     tts.setText("amogus")
+        //     tts.play()
+        // }, 2000)
+        
+    }, [])  
+
+    useEffect(() => {
+        if (!hasLoadedVoices) return
+        console.log("voices loaded", tts.voices)
+    
+        setTimeout(() => {
+            tts.setText("amogus")
+            tts.play()
+            console.log("lateee")
+        }, 2000)
+        // let msg = new SpeechSynthesisUtterance()
+        // window.speechSynthesis.cancel()
+        // msg.text = "Hello people"
+        // console.log("msg", msg)
+        // window.speechSynthesis.speak(msg)
+    }, [hasLoadedVoices])
+
+ 
+
+    // const [play, setPlay] = useState(false)
+    // const [ready2Play, setReady2Play] = useState(false)
+
+    // useEffect(() => {
+    //     if (!ready2Play) return
+    //     setPlay(true)
+
+    // }, [ready2Play])
+
     return (
         <>
-            <TTS 
-                text={"amogus"}
-            />
+            {/* <TTS
+                text={"amongus"}
+                play={play}
+                setPlay={setPlay}
+                setReady={setReady2Play}
+            /> */}
         </>
     )
 }
