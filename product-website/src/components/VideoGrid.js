d3d92fc323db9320677157fc99318e6d36fc5c02
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-05-14 17:44:15 +0100

    redirect to personality page

--- product-website/src/components/VideoGrid.js
+++ product-website/src/components/VideoGrid.js
@@ -36,34 +36,34 @@ function VideoGrid({ login,yourGames }) {
         <>
             <Row>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 1"} date={"22/03/2022"} />
                     </Link>
                 </Col>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 2"} date={"22/03/2022"} />
                     </Link>
                 </Col>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 3"} date={"21/03/2022"} />
                     </Link>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 4"} date={"21/03/2022"} />
                     </Link>
                 </Col>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 5"} date={"20/03/2022"} />
                     </Link>
                 </Col>
                 <Col>
-                    <Link to="/game_viewing" style={{ textDecoration: 'none' }}>
+                    <Link to="/personality" style={{ textDecoration: 'none' }}>
                         <VideoThumbnail login={login} Img={Img} title={"Video 6"} date={"20/03/2022"} />
                     </Link>
                 </Col>
