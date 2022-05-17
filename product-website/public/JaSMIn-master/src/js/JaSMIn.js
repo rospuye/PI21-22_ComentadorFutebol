03f005e8c042a49d509299bf4514d90e13dfdcc0
rospuye <66674322+rospuye@users.noreply.github.com>
2022-05-17 14:21:49 +0100

    Revert "Backend/statistics processing and modeling"

--- product-website/public/JaSMIn-master/src/js/JaSMIn.js
+++
@@ -1,7 +0,0 @@
-// Generate CSS file
-import '../scss/JaSMIn.scss';
-
-// Generate JS module
-export { Monitor } from './Monitor.js';
-export { MonitorParameters } from './MonitorParameters.js';
-export * from './Constants.js';
