8cdbaf45ce88d5a3194b2cebbc6ae41c1f31804b
rospuye <66674322+rospuye@users.noreply.github.com>
2022-05-20 16:18:06 +0100

    uploaded log file info

--- product-website/src/pages/UploadLogPage.js
+++ product-website/src/pages/UploadLogPage.js
@@ -30,17 +30,17 @@ function UploadLogPage() {
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
   const [privacy, setPrivacy] = useState("Private")
+  const [league, setLeague] = useState("")
+  const [year, setYear] = useState("")
+  const [round, setRound] = useState("")
+  const [matchGroup, setMatchGroup] = useState("")
 
   const [loading, setLoading] = useState(false)
 
   // tts states
   const [tts, setTts] = useState(new TTS())
   const [hasLoadedVoices, setHasLoadedVoices] = useState(false)
-  let hasButtonClicked = {value: false}
-
-  console.log("title: " + title)
-  console.log("description: " + description)
-  console.log("privacy: " + privacy)
+  let hasButtonClicked = { value: false }
 
   useEffect(() => {
     if (!tts.isSearchingVoices) {
@@ -55,41 +55,82 @@ function UploadLogPage() {
     tts.setVoice(voices_names[0])
   }, [hasLoadedVoices])
 
+  function validateFormField(field) {
+    if ((field.length > 0 && field.length <= 255)) {
+      return true
+    }
+    return false
+  }
+
   function processFile() {
     if (file) {
 
-      tts.speak("We are starting the convertion, please wait.", hasButtonClicked) // its necessary to create the initial speak
-      const url = 'http://127.0.0.1:8000/file_upload/';
-      const formData = new FormData();
-      formData.append('file', file);
-      formData.append('fileName', title);
-      formData.append('fileDescription', description);
-      formData.append('filePrivacy', privacy);
-      const config = {
-        headers: {
-          'content-type': 'multipart/form-data',
-          // 'Access-Control-Allow-Origin': 'http://localhost:3001'
-        },
-      };
-      document.getElementById('confirmBtn').disabled = true;
-      const spinner = "<Spinner animation=\"border\" role=\"status\"><span className=\"visually-hidden\">Loading...</span></Spinner>";
-      {/* <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner> */ }
-      // document.getElementById('confirmBtn').innerHTML = spinner;
-      setLoading(true)
-      axios.post(url, formData, config).then((response) => {
-        let data = response.data
-        console.log(data);
-        
-        for (let i = 0; i < 5; i++) {
-          tts.emmitAudio(data[i].text, hasButtonClicked)
+      if (validateFormField(title) && validateFormField(league) && validateFormField(round) && validateFormField(matchGroup)) {
+
+        tts.speak("We are starting the convertion, please wait.", hasButtonClicked) // its necessary to create the initial speak
+        const url = 'http://127.0.0.1:8000/file_upload/';
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('user', cookies.logged_user);
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('isPublic', privacy);
+        formData.append('league', league);
+        formData.append('year', year);
+        formData.append('round', round);
+        formData.append('matchGroup', matchGroup);
+        const config = {
+          headers: {
+            'content-type': 'multipart/form-data',
+            // 'Access-Control-Allow-Origin': 'http://localhost:3001'
+          },
+        };
+
+        document.getElementById('confirmBtn').disabled = true;
+
+        setLoading(true)
+        axios.post(url, formData, config).then((response) => {
+          let data = response.data
+          console.log(data);
+
+          for (let i = 0; i < 5; i++) {
+            tts.emmitAudio(data[i].text, hasButtonClicked)
+          }
+
+          document.getElementById('confirmBtn').disabled = false;
+          setLoading(false)
+        });
+
+      }
+      else {
+        if (!validateFormField(title)) {
+          document.getElementById("titleWarning").style.display = 'block';
         }
-        
-        document.getElementById('confirmBtn').disabled = false;
-        setLoading(false)
+        else {
+          document.getElementById("titleWarning").style.display = 'none';
+        }
 
+        if (!validateFormField(league)) {
+          document.getElementById("leagueWarning").style.display = 'block';
+        }
+        else {
+          document.getElementById("leagueWarning").style.display = 'none';
+        }
 
-        // document.getElementById('confirmBtn').innerHTML = "Confirm";
-      });
+        if (!validateFormField(round)) {
+          document.getElementById("roundWarning").style.display = 'block';
+        }
+        else {
+          document.getElementById("roundWarning").style.display = 'none';
+        }
+
+        if (!validateFormField(matchGroup)) {
+          document.getElementById("matchGroupWarning").style.display = 'block';
+        }
+        else {
+          document.getElementById("matchGroupWarning").style.display = 'none';
+        }
+      }
 
     }
     else {
@@ -107,12 +148,12 @@ function UploadLogPage() {
   // }
 
   return (<>
-      <div className='particlesBG'>
-      <ParticlesBg className="particles-bg-canvas-self" type="cobweb" bg={true} color="#DADADA"/>
-      </div>
-      <div style={{ padding: '1%' }}>
+    <div className='particlesBG'>
+      <ParticlesBg className="particles-bg-canvas-self" type="cobweb" bg={true} color="#DADADA" />
+    </div>
+    <div style={{ padding: '1%' }}>
       <Container>
-        <FocoNavbar goesBack={true} backPage="/select_game" hasLoginBtn={true} cookies={cookies} setCookie={setCookie}/>
+        <FocoNavbar goesBack={true} backPage="/select_game" hasLoginBtn={true} cookies={cookies} setCookie={setCookie} />
       </Container>
 
       <Container>
@@ -126,7 +167,7 @@ function UploadLogPage() {
       <Container>
         <Col style={{ marginLeft: '10%', marginRight: '10%' }}>
 
-          <Row style={{ marginTop: '5%', marginBottom: '5%', display: 'flex', justifyContent: 'center'}}>
+          <Row style={{ marginTop: '5%', marginBottom: '5%', display: 'flex', justifyContent: 'center' }}>
             <DragDrop parentCallback={handleCallback} />
           </Row>
 
@@ -140,49 +181,90 @@ function UploadLogPage() {
 
           {file ?
             <Row style={{ marginBottom: '5%' }}>
-              <Col style={{paddingLeft:'20%',paddingRight:'20%'}}>
-              <Container className='logUpload'>
-              <Form>
+              <Col style={{ paddingLeft: '20%', paddingRight: '20%' }}>
+                <Container className='logUpload'>
+                  <Form>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Title</Form.Label>
-                  <Form.Control type="text" placeholder="Enter title" defaultValue={file.name} onChange={(e) => { setTitle(e.target.value) }} />
-                </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Title</Form.Label>
+                      <Form.Control type="text" placeholder="Enter title" defaultValue={file.name} onChange={(e) => { setTitle(e.target.value) }} />
+                      {/* style={{ display: 'none' }} */}
+                      <Form.Text className="text-muted errorMessage" id="titleWarning" style={{ display: 'none' }}>
+                        This title is not long enough.
+                      </Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Label>Description</Form.Label>
-                  <FloatingLabel label="Comments">
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Enter Description"
-                      style={{ height: '100px' }}
-                      onChange={(e) => { setDescription(e.target.value) }}
-                    />
-                  </FloatingLabel>
-                </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Description</Form.Label>
+                      <FloatingLabel label="Comments">
+                        <Form.Control
+                          as="textarea"
+                          placeholder="Enter description"
+                          style={{ height: '100px' }}
+                          onChange={(e) => { setDescription(e.target.value) }}
+                        />
+                      </FloatingLabel>
+                    </Form.Group>
 
-                <Form.Group>
-                  <Form.Label>Video Privacy</Form.Label>
-                  <FloatingLabel>
-                    <Form.Select style={{ paddingTop: '0px', paddingBottom: '0px', height: '40px' }} onChange={(e) => { setPrivacy(e.target.value) }}>
-                      <option value="Private">Private</option>
-                      <option value="Public">Public</option>
-                    </Form.Select>
-                  </FloatingLabel>
-                </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Video Privacy</Form.Label>
+                      <FloatingLabel>
+                        <Form.Select style={{ paddingTop: '0px', paddingBottom: '0px', height: '40px' }} onChange={(e) => { setPrivacy(e.target.value) }}>
+                          <option value="Private">Private</option>
+                          <option value="Public">Public</option>
+                        </Form.Select>
+                      </FloatingLabel>
+                    </Form.Group>
 
-                <Form.Group style={{ textAlign: 'center', marginTop: '5%' }}>
-                  <Button id='confirmBtn' className='btnUpload' type="button" variant="primary" size="lg" style={{ width: '18%', margin: 'auto' }}  onClick={processFile}>
-                    {loading ?
-                      <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>
-                      :
-                      "Confirm"
-                    }
-                  </Button>
-                </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>League</Form.Label>
+                      <Form.Control type="text" placeholder="Enter league" onChange={(e) => { setLeague(e.target.value) }} />
+                      <Form.Text className="text-muted errorMessage" id="leagueWarning" style={{ display: 'none' }}>
+                        A value for the league is mandatory.
+                      </Form.Text>
+                    </Form.Group>
 
-              </Form>
-              </Container>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Year</Form.Label>
+                      <Form.Control id="game_year" type="number" min="1900" max={new Date().getFullYear()} step="1" defaultValue={new Date().getFullYear()} placeholder="Enter year" onChange={(e) => {
+                        if (e.target.value > new Date().getFullYear()) {
+                          document.getElementById("game_year").value = new Date().getFullYear();
+                        }
+                        else if (e.target.value < 1900) {
+                          document.getElementById("game_year").value = 1900;
+                        }
+                        setYear(e.target.value)
+                      }} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                      <Form.Label>Round</Form.Label>
+                      <Form.Control type="text" placeholder="Enter round" onChange={(e) => { setRound(e.target.value) }} />
+                      <Form.Text className="text-muted errorMessage" id="roundWarning" style={{ display: 'none' }}>
+                        A value for the round is mandatory.
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                      <Form.Label>Match group</Form.Label>
+                      <Form.Control type="text" placeholder="Enter match group" onChange={(e) => { setMatchGroup(e.target.value) }} />
+                      <Form.Text className="text-muted errorMessage" id="matchGroupWarning" style={{ display: 'none' }}>
+                        A value for the match group is mandatory.
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group style={{ textAlign: 'center', marginTop: '5%' }}>
+                      <Button id='confirmBtn' className='btnUpload' type="button" variant="primary" size="lg" style={{ width: '18%', margin: 'auto' }} onClick={processFile}>
+                        {loading ?
+                          <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>
+                          :
+                          "Confirm"
+                        }
+                      </Button>
+                    </Form.Group>
+
+                  </Form>
+                </Container>
               </Col>
             </Row>
             :
@@ -197,7 +279,7 @@ function UploadLogPage() {
         </Col>
       </Container>
     </div>
-    </>
+  </>
   )
 }
 
