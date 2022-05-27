2d844d9609610af7b6dc7af1d9f628e965230a84
luciusvinicius <luciusviniciusf@gmail.com>
2022-05-27 17:14:06 +0100

    onTimeChange and getPhraseByTime

--- product-website/src/pages/GameViewingPage.js
+++ product-website/src/pages/GameViewingPage.js
@@ -22,6 +22,31 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 // TTS
 import * as sdk from "microsoft-cognitiveservices-speech-sdk"
 
+const testScript = [
+    {"start": 3.1, "end": 8.3, "text": "Not implemented yet"},
+    {"start": 8.34, "end": 9.10002, "text": "matNum4matLeft has the ball!"},
+    {"start": 12.2601, "end": 12.2601, "text": "and the games goes on"},
+    {"start": 12.3001, "end": 13.8201, "text": "Not implemented yet :)"},
+    {"start": 13.8601, "end": 16.5002, "text": "matNum5matLeft has the ball!"},
+    {"start": 16.5002, "end": 16.5002, "text": "intersected the ball"},
+    {"start": 16.5402, "end": 18.6202, "text": "Not implemented yet :)"},
+    {"start": 18.6602, "end": 20.3003, "text": "matNum3matRight has the ball!"},
+    {"start": 20.3003, "end": 20.3003, "text": "intersected the ball"},
+    {"start": 20.7403, "end": 21.1803, "text": "matNum5matLeft and matNum3matRight fall down"},
+    {"start": 20.3403, "end": 22.3803, "text": "Not implemented yet :)"},
+    {"start": 22.1003, "end": 22.5803, "text": "matNum4matLeft and matNum8matRight fall down"},
+    {"start": 22.4203, "end": 25.2204, "text": "matNum3matLeft has the ball!"},
+    {"start": 25.7804, "end": 25.8204, "text": "matNum3matLeft has the ball!"},
+    {"start": 25.8604, "end": 25.9004, "text": "matNum3matLeft has the ball!"},
+    {"start": 26.2604, "end": 27.5804, "text": "matNum3matLeft is racing through the field"},
+    {"start": 27.5804, "end": 27.5804, "text": "intersected the ball"},
+    {"start": 27.6204, "end": 27.9804, "text": "matNum3matRight is racing through the field"},
+    {"start": 27.9804, "end": 27.9804, "text": "intersected the ball"},
+    {"start": 28.0205, "end": 28.0605, "text": "matNum3matLeft is racing through the field"},
+    {"start": 28.0605, "end": 28.0605, "text": "intersected the ball"},
+    {"start": 28.1005, "end": 28.1405, "text": "matNum3matLeft has the ball!"},
+    {"start": 28.1405, "end": 28.1405, "text": "intersected the ball"},
+]
 
 
 function commentaryToSSML(text, mood, diction, gender) {
@@ -68,11 +93,36 @@ function GameViewingPage() {
     const [mood, setMood] = useState("friendly")
     const [diction, setDiction] = useState(1)
     const [gender, setGender] = useState("female")
+    const [script, setScript] = useState([])
     const startPhrase = "Let's start the convertion."
+
     let gameTime = document.getElementsByClassName("game_time_lbl")
     let iframe = document.getElementById("video-game-iframe")
     console.log("time", gameTime)
 
+    const convertTime = (time="") => {
+        // Convert time to float value, instead of MM:SS.ss
+        const timeSplit = time.split(":")
+        const timeMin = timeSplit[0]/1 // str convertion lmao
+        const timeSeg = timeSplit[1]/1
+        return timeMin * 60 + timeSeg
+    }
+
+    const getPhraseByTimestamp = (time, errorMargin=0.05) => {
+        let test = script.filter((line) => {
+            return Math.abs(line.start - time) < errorMargin
+        })
+        console.log("test", test, time)
+    }
+
+    const onGameTimeChange = (mutations, observer) => {
+        gameTime = mutations[0].target.innerText
+        gameTime = convertTime(gameTime)
+        // TODO do stuff with gameTime
+        const phrase = getPhraseByTimestamp(gameTime)
+    }
+
+    const observer = new MutationObserver(onGameTimeChange)
 
     const initializeScript = () => {
         const speechConfig = sdk.SpeechConfig.fromSubscription("dfb5fa14bd85423db7a60da4b0ac369f", "westeurope");
@@ -82,11 +132,9 @@ function GameViewingPage() {
 
         iframe = document.getElementById("video-game-iframe")
         gameTime = iframe.contentWindow.document.getElementsByClassName("game_time_lbl")
-        if (gameTime.length !== 0)
-            gameTime = gameTime[0].innerText
-
-
-        console.log("time2", gameTime)
+        if (gameTime.length !== 0) {
+            observer.observe(gameTime[0], {characterData: false, childList: true, attributes: false})
+        }
 
         const ssml = commentaryToSSML(startPhrase, mood, diction, gender)
         console.log(ssml)
@@ -117,31 +165,6 @@ function GameViewingPage() {
             });
     }
 
-    const script = [
-        {"start": 3.1, "end": 8.3, "text": "Not implemented yet"},
-        {"start": 8.34, "end": 9.10002, "text": "matNum4matLeft has the ball!"},
-        {"start": 12.2601, "end": 12.2601, "text": "and the games goes on"},
-        {"start": 12.3001, "end": 13.8201, "text": "Not implemented yet :)"},
-        {"start": 13.8601, "end": 16.5002, "text": "matNum5matLeft has the ball!"},
-        {"start": 16.5002, "end": 16.5002, "text": "intersected the ball"},
-        {"start": 16.5402, "end": 18.6202, "text": "Not implemented yet :)"},
-        {"start": 18.6602, "end": 20.3003, "text": "matNum3matRight has the ball!"},
-        {"start": 20.3003, "end": 20.3003, "text": "intersected the ball"},
-        {"start": 20.7403, "end": 21.1803, "text": "matNum5matLeft and matNum3matRight fall down"},
-        {"start": 20.3403, "end": 22.3803, "text": "Not implemented yet :)"},
-        {"start": 22.1003, "end": 22.5803, "text": "matNum4matLeft and matNum8matRight fall down"},
-        {"start": 22.4203, "end": 25.2204, "text": "matNum3matLeft has the ball!"},
-        {"start": 25.7804, "end": 25.8204, "text": "matNum3matLeft has the ball!"},
-        {"start": 25.8604, "end": 25.9004, "text": "matNum3matLeft has the ball!"},
-        {"start": 26.2604, "end": 27.5804, "text": "matNum3matLeft is racing through the field"},
-        {"start": 27.5804, "end": 27.5804, "text": "intersected the ball"},
-        {"start": 27.6204, "end": 27.9804, "text": "matNum3matRight is racing through the field"},
-        {"start": 27.9804, "end": 27.9804, "text": "intersected the ball"},
-        {"start": 28.0205, "end": 28.0605, "text": "matNum3matLeft is racing through the field"},
-        {"start": 28.0605, "end": 28.0605, "text": "intersected the ball"},
-        {"start": 28.1005, "end": 28.1405, "text": "matNum3matLeft has the ball!"},
-        {"start": 28.1405, "end": 28.1405, "text": "intersected the ball"},
-    ]
 
 
 
@@ -160,6 +183,9 @@ function GameViewingPage() {
     //     console.log("End")
     // }, [flg])
 
+    useEffect(() => {
+        setScript(testScript)
+    }, [])
 
 
     return (
