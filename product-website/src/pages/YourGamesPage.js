8b25026aa4269b1c602c2af0d45f37826bec62da
luciusvinicius <luciusviniciusf@gmail.com>
2022-06-10 11:25:21 +0100

    edit page working 100%

--- product-website/src/pages/YourGamesPage.js
+++ product-website/src/pages/YourGamesPage.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Button, Spinner } from 'react-bootstrap'
 import SearchBox from '../components/SearchBox'
 import SortInput from '../components/SortInput'
 import Title from '../components/Title'
@@ -7,7 +7,7 @@ import Title from '../components/Title'
 import '../components/components_css/Form.css';
 import VideoGrid from '../components/VideoGrid'
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import ParticlesBg from 'particles-bg';
@@ -29,8 +29,15 @@ function YourGamesPage() {
     const [selectedRound,setSelectedRound] = useState("")
     const [selectedGroup,setSelectedGroup] = useState("")
     const [selectedTitle,setSelectedTitle] = useState("")
+    const [selectedPage, setSelectedPage] = useState(1)
+    const [numberOfPages, setNumberOfPages] = useState(0)
+    const [isLoaded, setIsLoaded] = useState(false)
+
 
     const [games, setGames] = useState([])
+
+    const navigate = useNavigate();
+
 
     const requestGame = () => {
         let url = process.env.REACT_APP_API_URL + `games?`
@@ -43,9 +50,7 @@ function YourGamesPage() {
             url += `league=${selectedLeague}&`
         }
 
-        if (selectedUser != "") {
-            url += `username=${selectedUser}&`
-        }
+        url += `username=${cookies.logged_user}&`
 
         if (selectedYear != "") {
             url += `year=${selectedYear}&`
@@ -65,6 +70,8 @@ function YourGamesPage() {
         .then(res => {
             console.log(res.data)
             setGames(res.data.results)
+            setNumberOfPages(res.data.total_pages)
+            setIsLoaded(true)
         })
     }
     const updateLogin = () => {
@@ -72,6 +79,14 @@ function YourGamesPage() {
         setLogin(cookies.logged_user !== '')
         window.location.reload()
     }
+
+    useEffect(() => {
+        if (cookies.logged_user === "" || cookies.logged_user === null) {
+            navigate("/")
+            return
+        }
+        requestGame()
+    }, [])
 
     return (
         <>
@@ -90,41 +105,52 @@ function YourGamesPage() {
                 </Row>
             </Container>
 
-            <Container>
-                <Row style={{ marginTop: '3%' }}>
-                    <Col xs={3}>
-                        {/* Filters and Buttons */}
-                        <SearchBox 
-                            login={login} 
-                            selectedLeague={selectedLeague}
-                            setSelectedLeague={setSelectedLeague}
-                            selectedUser={selectedUser}
-                            setSelectedUser={setSelectedUser}
-                            selectedYear={selectedYear}
-                            setSelectedYear={setSelectedYear}
-                            selectedRound={selectedRound}
-                            setSelectedRound={setSelectedRound}
-                            selectedGroup={selectedGroup}
-                            setSelectedGroup={setSelectedGroup}
-                            selectedTitle={selectedTitle}
-                            setSelectedTitle={setSelectedTitle}
-                            requestGame={requestGame}
-                        />
-                    </Col>
-                    <Col xs={9}>
-                        <Row style={{display:'flex',alignItems:'center',justifyContent:'right',marginBottom:'3%'}}>
-                            <Container style={{width:'60%',marginLeft:'30%'}}>
-                                <SortInput />
-                            </Container>
-                        </Row>
-                        <Row style={{ marginTop: '20px' }}>
-                        <VideoGrid login={true} yourGames={true}/>
-                        </Row>
-                        {/* <VideoGrid games={null}/>
-                        Grid with videos and pages */}
-                    </Col>
-                </Row>
-            </Container>
+            {isLoaded ?
+                <Container>
+                    <Row style={{ marginTop: '3%' }}>
+                        <Col xs={3}>
+                            {/* Filters and Buttons */}
+                            <SearchBox 
+                                login={login} 
+                                selectedLeague={selectedLeague}
+                                setSelectedLeague={setSelectedLeague}
+                                selectedUser={selectedUser}
+                                setSelectedUser={setSelectedUser}
+                                selectedYear={selectedYear}
+                                setSelectedYear={setSelectedYear}
+                                selectedRound={selectedRound}
+                                setSelectedRound={setSelectedRound}
+                                selectedGroup={selectedGroup}
+                                setSelectedGroup={setSelectedGroup}
+                                selectedTitle={selectedTitle}
+                                setSelectedTitle={setSelectedTitle}
+                                requestGame={requestGame}
+                            />
+                        </Col>
+                        <Col xs={9}>
+                            <Row style={{display:'flex',alignItems:'center',justifyContent:'right',marginBottom:'3%'}}>
+                                <Container style={{width:'60%',marginLeft:'30%'}}>
+                                    <SortInput />
+                                </Container>
+                            </Row>
+                            <Row style={{ marginTop: '20px' }}>
+                                <VideoGrid 
+                                    // login={true} 
+                                    // yourGames={true}
+                                    games={games}
+                                    isEditable
+                                />
+                            </Row>
+                            {/* <VideoGrid games={null}/>
+                            Grid with videos and pages */}
+                        </Col>
+                    </Row>
+                </Container>
+            :
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "10em"}}>
+                    <Spinner animation={"border"} variant={"primary"} />
+                </div>
+            }
             </div>
         </>
     )
